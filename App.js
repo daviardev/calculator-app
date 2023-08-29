7bb5f2086272e1a28a944675ee8e9ad8cb2d0a1f
daviardev <jerson.silva99@hotmail.com>
2023-08-28 20:19:00 -0500

    Upload initial files

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './src/styles/HomeStyles.js'
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calculator App</Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
